c3338bce00bcd34d4cd8f301e9ff113f44528246
inwoo0808 <inwoo0808@gmail.com>
2024-09-04 10:00:18 +0900

    9/4까지 실습

---
+++ 03vue/vuetest/vuetest1/src/App.vue
@@ -0,0 +1,127 @@
+<template>
+   <NavbarView />
+   <SearchBarView :data="data" />
+   <ComponentView :data="data" @openModal="modalOpen" />
+   <ModalView :data="data" :isModal="isModal" :num="selectedNum" @closeModal="isModal = false" />
+</template>
+
+<script>
+import mdata from './assets/mdata';
+import NavbarView from './components/NavbarView.vue';
+import ModalView from './components/ModalView.vue';
+import ComponentView from './components/ComponentView.vue';
+import SearchBarView from './components/SearchBarView.vue';
+
+export default {
+   name: 'appView',
+   data() {
+      return {
+         data: mdata,
+         isModal: false,
+         selectedNum: 0,
+      };
+   },
+   methods: {
+      increseLike(i) {
+         console.log(i);
+         this.data[i].like += 1;
+      },
+      modalOpen(num) {
+         this.isModal = true;
+         this.selectedNum = num;
+         console.log('view', num);
+      },
+      closeM() {
+         this.isModal = false;
+      },
+   },
+   components: {
+      NavbarView: NavbarView,
+      ModalView: ModalView,
+      ComponentView: ComponentView,
+      SearchBarView: SearchBarView,
+   },
+};
+</script>
+
+<style lang="scss">
+$radius: 5px;
+.container {
+   width: 1000px;
+   margin: 0 auto;
+
+   @media screen and (max-width: 790px) {
+      width: 100%;
+      padding: 0 16px;
+   }
+   img {
+      display: block;
+   }
+}
+
+.cardWrap {
+   display: flex;
+   flex-wrap: wrap;
+   gap: 10px;
+   .card {
+      width: calc((100% - 40px) / 5);
+      // width: 25%;
+
+      @media screen and (max-width: 790px) {
+         width: calc((100% - 10px) / 2);
+      }
+
+      @media screen and (max-width: 560px) {
+         width: 100%;
+      }
+
+      .imgWrap {
+         width: 100%;
+         position: relative;
+         overflow: hidden;
+         cursor: pointer;
+         border-radius: $radius;
+         img {
+            width: 100%;
+            transition: 0.3s;
+         }
+
+         &:hover img {
+            transform: scale(1.1);
+         }
+         .hot {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background-color: skyblue;
+            padding: 2px 10px;
+            border-radius: 5px;
+            font-weight: bold;
+            color: white;
+         }
+      }
+   }
+}
+.mb-10 {
+   margin-bottom: 10px !important;
+}
+
+.btn {
+   background: pink;
+   border-radius: $radius;
+   padding: 5px 16px;
+   text-align: center;
+   cursor: pointer;
+   color: white;
+   border: 0 none;
+   display: block;
+   width: 100%;
+
+   &.btn-primary {
+      background-color: skyblue;
+   }
+   &.btn-info {
+      background-color: greenyellow;
+   }
+}
+</style>
